ade7de3a11f722a4f55701b5e9d63771d7deb62a
Phongnhv <145695401+Phongnhv@users.noreply.github.com>
2024-09-30 10:01:48 +0700

    Update search.py

--- src/main/java/com/example/mrs/pythonProgramm/search.py
+++ src/main/java/com/example/mrs/pythonProgramm/search.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from collections import defaultdict
 import sys
 
-client_id = '64beda4e6b21451283236269ef10aaec'
-client_secret = '4f238894f89e43df8b9a5ca2cf977a30'
+client_id = 'CLIENT_ID'
+client_secret = 'CLIENT_SECRET'
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
